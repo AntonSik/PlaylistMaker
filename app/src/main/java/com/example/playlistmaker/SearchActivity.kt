223d287bb38752eb97515b94539b43a3ef175ae7
Anton <boroda_c_m9com@mail.ru>
2024-02-15 21:36:08 +0300

    the second task first commit

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,75 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+
 
 class SearchActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val imageArrow = findViewById<ImageView>(R.id.arrow2)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        imageArrow.setOnClickListener{
+           finish()
+        }
+
+        clearButton.setOnClickListener{
+            inputEditText.setText("")
+        }
+// EmptyTextWartcher
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val input = p0.toString()
+                clearButton.visibility = clearButtonVisibility(input)
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //empty
+            }
+
+        }
+
+        inputEditText.addTextChangedListener(textWatcher)
+
+        inputEditText.setText(savedText)
+    }
+
+    private var savedText = VALUE
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT, savedText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedText = savedInstanceState.getString(INPUT_TEXT, VALUE)
+
+
+    }
+    companion object {
+        const val INPUT_TEXT = "INPUT_EDIT"
+        const val VALUE = " "
+    }
+}
+private fun clearButtonVisibility(s: CharSequence?): Int {
+    return if (s.isNullOrEmpty()) {
+        View.GONE
+    } else {
+        View.VISIBLE
     }
 }
