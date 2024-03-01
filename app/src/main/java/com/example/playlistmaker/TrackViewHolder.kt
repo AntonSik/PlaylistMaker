6399b4c24194d68e94763b5028248e239d0dede5
Anton <boroda_c_m9com@mail.ru>
2024-03-01 17:26:30 +0300

    the third task third commit

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,7 +1,30 @@
 package com.example.playlistmaker
 
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
 
-class TrackViewHolder (itemView : View): RecyclerView.ViewHolder(itemView){
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val ivPrint = itemView.findViewById<ImageView>(R.id.iv_print)
+    private val tvTiming = itemView.findViewById<TextView>(R.id.tv_timing)
+    private val tvTrack: TextView = itemView.findViewById(R.id.tv_track)
+    private val tvAuthor: TextView = itemView.findViewById(R.id.tv_author)
+
+    fun bind(item: Track) {
+        val url = item.artworkUrl100
+        Glide.with(itemView)
+            .load(url)
+            .placeholder(R.drawable.not_found_icon)
+            .fitCenter()
+            .transform(RoundedCorners(10))
+            .into(ivPrint)
+        tvTrack.text = item.trackName
+        tvAuthor.text = item.artistName
+        tvTiming.text = item.trackTime
+
+    }
 }
