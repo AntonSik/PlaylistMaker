a2215bc5b80150e2d76fb5bfeb102f657800c638
AntonSik <121441647+AntonSik@users.noreply.github.com>
2024-04-14 14:09:46 +0300

    Fixing and adapting (#7)
    * test-fixing branch
    
    * test-fixing branch second commit

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -3,6 +3,7 @@ package com.example.playlistmaker
 import android.content.Context
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -19,7 +20,9 @@ class AudioPlayerActivity : AppCompatActivity() {
     private lateinit var artistsTrack: TextView
     private lateinit var durationTrack: TextView
     private lateinit var albumTrack: TextView
+    private lateinit var album: TextView
     private lateinit var yearTrack: TextView
+    private lateinit var year: TextView
     private lateinit var genreTrack: TextView
     private lateinit var countryTrack: TextView
     private val dateFormat by lazy { SimpleDateFormat("mm:ss", Locale.getDefault()) }
@@ -31,14 +34,7 @@ class AudioPlayerActivity : AppCompatActivity() {
         val selectedTrack = intent.getParcelableExtra<Track>("clicked track")
 
         val imageArrow = findViewById<ImageButton>(R.id.ib_arrowBack)
-        coverTrack = findViewById(R.id.iv_cover)
-        nameTrack = findViewById(R.id.tv_trackName)
-        artistsTrack = findViewById(R.id.tv_artists)
-        durationTrack = findViewById(R.id.tv_enterDuration)
-        albumTrack = findViewById(R.id.tv_enterAlbum)
-        yearTrack = findViewById(R.id.tv_enterYear)
-        genreTrack = findViewById(R.id.tv_enterGenre)
-        countryTrack = findViewById(R.id.tv_enterCountry)
+        init()
         bind(selectedTrack)
 
         imageArrow.setOnClickListener { finish() }
@@ -46,8 +42,8 @@ class AudioPlayerActivity : AppCompatActivity() {
 
     }
 
-    fun bind(track: Track?) {
-        val url = track?.artworkUrl100?.replaceAfterLast('/',"512x512bb.jpg")
+    private fun bind(track: Track?) {
+        val url = track?.artworkUrl100?.replaceAfterLast('/', "512x512bb.jpg")
         Glide.with(this)
             .load(url)
             .placeholder(R.drawable.placeholder)
@@ -57,17 +53,44 @@ class AudioPlayerActivity : AppCompatActivity() {
         nameTrack.text = track?.trackName
         artistsTrack.text = track?.artistName
         durationTrack.text = dateFormat.format(track?.trackTimeMillis)
-        albumTrack.text = track?.collectionName
+        if (track?.collectionName.isNullOrEmpty()) {
+            albumTrack.visibility = View.GONE
+            album.visibility = View.GONE
+        } else {
+            albumTrack.text = track?.collectionName
+            albumTrack.visibility = View.VISIBLE
+            album.visibility = View.VISIBLE
+        }
         genreTrack.text = track?.primaryGenreName
-        yearTrack.text = track?.releaseData
-        countryTrack.text =track?.country
+        if (track?.releaseDate.isNullOrEmpty()) {
+            yearTrack.visibility = View.GONE
+            year.visibility = View.GONE
+        } else {
+            yearTrack.text = track?.releaseDate?.substring(0, 4)
+            yearTrack.visibility = View.VISIBLE
+            year.visibility = View.VISIBLE
+        }
+        countryTrack.text = track?.country
     }
 
-    fun dpToPx(dp: Float, context: Context): Int {
+    private fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
             dp,
             context.resources.displayMetrics
         ).toInt()
+    }
+
+    private fun init() {
+        coverTrack = findViewById(R.id.iv_cover)
+        nameTrack = findViewById(R.id.tv_trackName)
+        artistsTrack = findViewById(R.id.tv_artists)
+        durationTrack = findViewById(R.id.tv_enterDuration)
+        albumTrack = findViewById(R.id.tv_enterAlbum)
+        album = findViewById(R.id.tv_album)
+        yearTrack = findViewById(R.id.tv_enterYear)
+        year = findViewById(R.id.tv_year)
+        genreTrack = findViewById(R.id.tv_enterGenre)
+        countryTrack = findViewById(R.id.tv_enterCountry)
     }
 }
